722de63affb72f9df48529ca8b850e7d9b96a9e0
据说甜蜜呢 <lijun@geekbuy.com.cn>
2019-02-13 17:50:26 +0800

    添加方法

--- BatchProcessingEngine/Extensions/ProcessingContextExtension.cs
+++ BatchProcessingEngine/Extensions/ProcessingContextExtension.cs
@@ -3,7 +3,7 @@
     public static class ProcessingContextExtension
     {
         /// <summary>
-        /// 获取并行轮数
+        /// 获取并行处理轮数
         /// </summary>
         /// <param name="context">处理上下文</param>
         public static int GetParallelRounds(this ProcessingContext context)
@@ -17,6 +17,19 @@
             var batchSize = context.LargeBatch.BatchSize;
 
             return totalSize % batchSize == 0 ? totalSize / batchSize : (totalSize / batchSize) + 1;
+        }
+
+        /// <summary>
+        /// 克隆 - 深复制
+        /// </summary>
+        public static ProcessingContext Copy(this ProcessingContext context)
+        {
+            return new ProcessingContext()
+            {
+                TotalThroughput = context.TotalThroughput,
+                LargeBatch = context.LargeBatch,
+                SmallBatch = context.SmallBatch
+            };
         }
     }
 }

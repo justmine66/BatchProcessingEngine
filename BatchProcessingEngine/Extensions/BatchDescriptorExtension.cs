1f590f41f72d2824c9115e5e9416614c1149c6a0
justmine <lijun@geekbuy.com.cn>
2019-03-15 18:46:44 +0800

    添加文件

---
+++ BatchProcessingEngine/Extensions/BatchDescriptorExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace BatchProcessingEngine.Extensions
+{
+    public static class BatchDescriptorExtension
+    {
+        public static int LargeBatchLastId(this BatchDescriptor largeBatch, int batchSize)
+        {
+            return largeBatch.CheckPointId + (largeBatch.BatchSequence - 1) * batchSize;
+        }
+    }
+}

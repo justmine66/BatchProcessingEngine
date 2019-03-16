ca6586d3d9834f9aa8b75ddf468b933d69357927
justmine <lijun@geekbuy.com.cn>
2019-03-16 15:15:19 +0800

    添加文件

---
+++ Samples/BatchETL/InMemoryDb.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BatchETL
+{
+    internal static class InMemoryDb
+    {
+        internal static List<PayLoad> Payloads = new List<PayLoad>();
+
+        static InMemoryDb()
+        {
+            for (var i = 1; i <= 400; i++)
+                Payloads.Add(new PayLoad() { Id = i, Content = $"第{i}条内容" });
+        }
+    }
+}
---
+++ Samples/BatchETL/MicroBatchDataProvider.cs
@@ -0,0 +1,33 @@
+﻿using BatchProcessingEngine;
+using BatchProcessingEngine.Extensions;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BatchETL
+{
+    public class MicroBatchDataProvider : IDataProvider
+    {
+        public async Task<dynamic> GetBatchDataAsync(ProcessingContext context)
+        {
+            var batchSize = context.MetaData.SmallBatch.BatchSize;
+            var offsetFrom = context.BatchOffsetFrom();
+            var offsetTo = offsetFrom + batchSize;
+
+            var payloads = InMemoryDb.Payloads
+                .Where(it => it.Id >= offsetFrom && it.Id < offsetTo)
+                .Take(batchSize);
+
+            return payloads;
+        }
+
+        public Task<int> GetCheckPointIdAsync()
+        {
+            return Task.FromResult(InMemoryDb.Payloads.First().Id);
+        }
+
+        public Task<int> GetTotalSizeAsync()
+        {
+            return Task.FromResult(InMemoryDb.Payloads.Count);
+        }
+    }
+}

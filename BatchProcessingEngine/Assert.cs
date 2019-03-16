b61ebd46b9aa8bd30d90451c3454defd81e5d7d9
justmine <lijun@geekbuy.com.cn>
2019-03-16 15:12:27 +0800

    添加断言类

---
+++ BatchProcessingEngine/Assert.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BatchProcessingEngine
+{
+    public static class Assert
+    {
+        public static void Range(float value, int from, int to)
+        {
+            if (value > to || value <= from)
+                throw new ArgumentOutOfRangeException($"{nameof(value)}[{value}] must be greater than {from} and less than or equal to {to}.");
+        }
+    }
+}

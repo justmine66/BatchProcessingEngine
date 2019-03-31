6f080a788f9cc5781f79121ab8c318d87f7fc86c
justmine <justmine@DESKTOP-UILU6SC>
2019-03-31 13:17:13 +0800

    添加方法NotNull

--- BatchProcessingEngine/Assert.cs
+++ BatchProcessingEngine/Assert.cs
@@ -4,6 +4,12 @@ namespace BatchProcessingEngine
 {
     public static class Assert
     {
+        public static void NotNull(object obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+        }
+
         public static void Range(float value, int from, int to)
         {
             if (value > to || value <= from)

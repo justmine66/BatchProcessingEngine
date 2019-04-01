1f90b3c3349af458498a9d02c6a61cb83306639b
justmine <lijun@geekbuy.com.cn>
2019-04-01 11:07:33 +0800

    添加configure

--- BatchProcessingEngine/Extensions/ServiceCollectionExtension.cs
+++ BatchProcessingEngine/Extensions/ServiceCollectionExtension.cs
@@ -1,16 +1,21 @@
-﻿using System;
-using BatchProcessingEngine.Configuration;
+﻿using BatchProcessingEngine.Configuration;
 using BatchProcessingEngine.Eventing;
 using BatchProcessingEngine.WorkPool;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
+using System;
 
 namespace BatchProcessingEngine.Extensions
 {
     public static class ServiceCollectionExtension
     {
         public static IServiceCollection AddProcessingServices(this IServiceCollection container)
+        {
+            return AddProcessingServices(container, builder => { });
+        }
+
+        public static IServiceCollection AddProcessingServices(this IServiceCollection container, Action<IBatchProcessingEngineBuilder> configure)
         {
             if (container == null)
                 throw new ArgumentNullException(nameof(container));
@@ -30,6 +35,8 @@ namespace BatchProcessingEngine.Extensions
             container.AddSingleton<IProcessingPipeLineBuilder, ProcessingPipeLineBuilder>();
 
             container.AddSingleton<IApplicationListenerSource, ApplicationListenerSourceImpl>();
+
+            configure(new BatchProcessingEngineBuilder(container));
 
             return container;
         }
